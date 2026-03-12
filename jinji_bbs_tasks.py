3e804c347cdea0c4a71076628cc9c097d05c8749
mikiozaki-boop <miki_ozaki@hokenlabo.net>
2026-03-12 11:20:14 +0900

    Update jinji_bbs_tasks.py

--- jinji_bbs_tasks.py
+++ jinji_bbs_tasks.py
@@ -6,23 +6,12 @@ from datetime import datetime, timedelta
 API_TOKEN = os.environ["CHATWORK_API_TOKEN"]
 ROOM_ID = os.environ["CHATWORK_ROOM_ID"]
 
-TASKS = [
-    {
-        "name": "安田璃奈子",
-        "accountId": "4374516",
-        "body": "【連絡】休暇報告書提出のお願い\n今月の休暇報告書を期限までに提出してください。提出先はグループチャット「人事BBS」にお願いします。\n（休暇報告書格納先：https://www.chatwork.com/#!rid359970432-2033482443050455040）",
-        "timing": "monthly",
-        "day": 15,
-    },
-    {
-        "name": "島仲弘英",
-        "accountId": "8390505",
-        "body": "【連絡】休暇報告書提出のお願い\n今月の休暇報告書を期限までに提出してください。提出先はグループチャット「人事BBS」にお願いします。\n（休暇報告書格納先：https://www.chatwork.com/#!rid359970432-2033482443050455040）",
-        "timing": "monthly",
-        "day": 15,
-    },
-   
+MEMBERS = [
+    {"name": "安田璃奈子", "accountId": "4374516"},
+    {"name": "島仲弘英",   "accountId": "8390505"},
 ]
+
+TASK_BODY = "【連絡】休暇報告書提出の期限のお知らせ\n今月の休暇報告書を期限までに提出してください。提出先はグループチャット「人事BBS」にお願いします。\n（休暇報告書格納先：https://www.chatwork.com/#!rid359970432-2033482443050455040）"
 
 def get_limit_date(year, month):
     base = datetime(year, month, 20)
@@ -35,31 +24,34 @@ def get_limit_date(year, month):
                 return d
         d += timedelta(days=1)
 
-def create_task(task, limit_timestamp):
+def create_task(member, limit_timestamp):
     url = "https://api.chatwork.com/v2/rooms/" + ROOM_ID + "/tasks"
     res = requests.post(
         url,
         headers={"X-ChatWorkToken": API_TOKEN},
         data={
-            "body": task["body"],
-            "to_ids": task["accountId"],
+            "body": TASK_BODY,
+            "to_ids": member["accountId"],
             "limit_type": "date",
             "limit": limit_timestamp
         }
     )
     if res.status_code == 200:
-        print(task["name"] + ": OK")
+        print(member["name"] + ": OK")
     else:
-        print(task["name"] + ": NG " + res.text)
+        print(member["name"] + ": NG " + res.text)
 
 def main():
     now = datetime.now()
+    if now.day != 15:
+        print("今日は15日ではないため実行しません")
+        return
+
     limit_day = get_limit_date(now.year, now.month)
     limit_timestamp = int(datetime(limit_day.year, limit_day.month, limit_day.day, 23, 59, 59).timestamp())
 
-    for task in TASKS:
-        if task["timing"] == "monthly" and task["day"] == now.day:
-            create_task(task, limit_timestamp)
+    for member in MEMBERS:
+        create_task(member, limit_timestamp)
 
 if __name__ == "__main__":
     main()
