ad681b5ed47fb0703893320c9d9b7164b7e8740f
mikiozaki-boop <miki_ozaki@hokenlabo.net>
2026-03-12 10:59:18 +0900

    Update jinji_bbs_tasks.py

--- jinji_bbs_tasks.py
+++ jinji_bbs_tasks.py
@@ -1,6 +1,7 @@
 import os
 import requests
-from datetime import datetime
+import jpholiday
+from datetime import datetime, timedelta
 
 API_TOKEN = os.environ["CHATWORK_API_TOKEN"]
 ROOM_ID = os.environ["CHATWORK_ROOM_ID"]
@@ -11,29 +12,36 @@ TASKS = [
         "accountId": "4374516",
         "body": "【連絡】休暇報告書提出の期限のお知らせ\n今月の休暇報告書を期限までに提出してください。提出先はグループチャット「人事BBS」にお願いします。\n（休暇報告書格納先：https://www.chatwork.com/#!rid359970432-2033482443050455040）",
         "timing": "monthly",
-        "day": 16,
+        "day": 15,
     },
     {
         "name": "島仲弘英",
         "accountId": "8390505",
         "body": "【連絡】休暇報告書提出の期限のお知らせ\n今月の休暇報告書を期限までに提出してください。提出先はグループチャット「人事BBS」にお願いします。\n（休暇報告書格納先：https://www.chatwork.com/#!rid359970432-2033482443050455040）",
         "timing": "monthly",
-        "day": 16,
+        "day": 15,
     },
     {
         "name": "尾崎美希",
         "accountId": "2769500",
         "body": "【連絡】休暇報告書提出の期限のお知らせ\n今月の休暇報告書を期限までに提出してください。提出先はグループチャット「人事BBS」にお願いします。\n（休暇報告書格納先：https://www.chatwork.com/#!rid359970432-2033482443050455040）",
         "timing": "monthly",
-        "day": 16,
+        "day": 15,
     },
 ]
 
-def create_task(task):
-    now = datetime.now()
-    limit_date = datetime(now.year, now.month, 23, 23, 59, 59)
-    limit_timestamp = int(limit_date.timestamp())
+def get_limit_date(year, month):
+    base = datetime(year, month, 20)
+    count = 0
+    d = base + timedelta(days=1)
+    while count < 2:
+        if d.weekday() < 5 and not jpholiday.is_holiday(d):
+            count += 1
+            if count == 2:
+                return d
+        d += timedelta(days=1)
 
+def create_task(task, limit_timestamp):
     url = "https://api.chatwork.com/v2/rooms/" + ROOM_ID + "/tasks"
     res = requests.post(
         url,
@@ -52,9 +60,12 @@ def create_task(task):
 
 def main():
     now = datetime.now()
+    limit_day = get_limit_date(now.year, now.month)
+    limit_timestamp = int(datetime(limit_day.year, limit_day.month, limit_day.day, 23, 59, 59).timestamp())
+
     for task in TASKS:
         if task["timing"] == "monthly" and task["day"] == now.day:
-            create_task(task)
+            create_task(task, limit_timestamp)
 
 if __name__ == "__main__":
     main()
